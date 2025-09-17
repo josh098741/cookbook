3274261ffdcf2801fbda87d202b5fd860270d851
josh098741 <morongemokaya@gmail.com>
2025-09-17 03:01:22 +0300

    Additional Commits

--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -1,11 +1,11 @@
-import profilePic from './assets/josh.png'
+import profileImage from './assets/josh.png'
 
 function Card(){
     return(
         <div className="card">
-            <img className="card-image" src={profilePic} alt="Profile Picture"></img>
-            <h2 className="card-title">Joshua Moronge</h2>
-            <p className="card-text">I study at Jkuat and play video games</p>
+            <img src={profileImage} className="card-image" alt="Profile Image"></img>
+            <h1 className="card-header">Joshua Moronge</h1>
+            <p className="card-text">Im A student at Jkuat</p>
         </div>
     );
 }
