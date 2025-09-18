72a8bb5ef49c0bec0c368b8f2fc750a07e0a88fa
josh098741 <morongemokaya@gmail.com>
2025-09-18 12:40:49 +0300

    Additional commits

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,13 @@
-import Card from './Card'
+import Student from './Student'
 
 function App() {
   return(
     <>
-      <Card />
+      <Student name="Spongebob Squarepants" age={30} isStudent={true}/>
+      <Student name="Patrick Starfish" age={42} isStudent={false}/>
+      <Student name="Squidward" age={50} isStudent={false}/>
+      <Student name="Sandy Cheeks" age={24} isStudent={true}/>
+      <Student name="Larry"/>
     </>
   );
 }
